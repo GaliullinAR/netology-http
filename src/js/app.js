401d54c79801bdb130e4c7443fd34b3b3dddd229
GaliullinAR <galiullin.ar651@gmail.com>
2023-08-01 16:17:34 +0300

    add all files

--- src/js/app.js
+++ src/js/app.js
@@ -5,9 +5,9 @@ import getEditTicketWidget from "./editTicketWidget";
 import changeTicketStatus from "./functionChangeTicketStatus";
 import showTicketDescription from "./functionShowTicketDescription";
 
-// const port = 7070;// for devserver  work
+// const port = 8080;// for devserver  work
 // const serverUrl = `http://localhost:${port}`;// for devserver  work
-const serverUrl = "https://ahj-hw-http.herokuapp.com/";
+// const serverUrl = "https://ahj-hw-http.herokuapp.com/";
 
 const mainContainer = document.querySelector(".container");
 const ticketsContainer = document.querySelector(".tickets-container");
